189763cb22970cf6eb968524c66a85c2061611f1
parthtech221 <parthtech221@gmail.com>
2026-08-21 12:27:44 +0530

    add file

---
+++ 01_Array/002_find_min.py
@@ -0,0 +1,14 @@
+# Problem: Find Minimum Element
+# Difficulty: Easy
+# Topic: Arrays
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+def find_min(arr):
+    min = arr[0]
+    for i in arr:
+        if min > i:
+            min = i
+    return i
+
+print(find_min([3,7,2,9,1]))        
