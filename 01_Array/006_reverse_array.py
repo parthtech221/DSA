4b92412069b25f780e2d5908a289742f70fc3c89
parthtech221 <parthtech221@gmail.com>
2026-08-21 14:10:58 +0530

    add new file

---
+++ 01_Array/006_reverse_array.py
@@ -0,0 +1,16 @@
+# Problem: revrerse array
+# Difficulty: Easy
+# Topic: Arrays
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+def revers(arr):
+    n=len(arr)-1
+    rev=[]
+    for i in range(n,-1,-1):
+        rev.append(arr[i])
+    return rev
+    
+
+print(revers([1,2,3,5]))    
+        
