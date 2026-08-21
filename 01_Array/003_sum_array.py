5977f53934f09aca3f023d9889a92c26dc67ddd8
parthtech221 <parthtech221@gmail.com>
2026-08-21 12:31:41 +0530

    python

---
+++ 01_Array/003_sum_array.py
@@ -0,0 +1,13 @@
+# Problem: Find sum of element
+# Difficulty: Easy
+# Topic: Arrays
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+def sum(arr):
+    sum=0
+    for i in arr:
+        sum+=i
+    return sum    
+
+print(sum([1, 2, 3, 4]))
