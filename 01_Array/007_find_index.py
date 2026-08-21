0624bd3194612a6e97e058d480a19a17aeec3b46
parthtech221 <parthtech221@gmail.com>
2026-08-21 14:20:21 +0530

    add file

---
+++ 01_Array/007_find_index.py
@@ -0,0 +1,15 @@
+# Problem: find index of element
+# Difficulty: Easy
+# Topic: Arrays
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+def findindex(arr,target):
+    index = -1
+    for i in arr:
+        index += 1
+        if target==i:
+            break
+    return index
+
+print(findindex([10,20,30,40],30))    
