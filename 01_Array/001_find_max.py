937b1ea0fca3e84ccbf9ffcbdce6fd13c6c9549e
parthtech221 <parthtech221@gmail.com>
2026-08-21 12:23:18 +0530

    update

--- 01_Array/001_find_max.py
+++ 01_Array/001_find_max.py
@@ -1,1 +1,14 @@
-print("hi")
+# Problem: Find Maximum Element
+# Difficulty: Easy
+# Topic: Arrays
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+def find_max(arr):
+    max = arr[0]
+    for i in arr:
+        if max < i:
+            max = i
+    return max
+
+print(find_max([3, 7, 2, 9, 1]))
