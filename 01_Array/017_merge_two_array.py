902c78e8b252a436da202b3dd7ef0fc54330d4be
parthtech221 <parthtech221@gmail.com>
2026-08-22 20:39:48 +0530

    add file

---
+++ 01_Array/017_merge_two_array.py
@@ -0,0 +1,15 @@
+#problem: merge two array
+# Difficulty: Easy
+# Topic: Arrays
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+def merge_arr(a,b):
+    new_arr = []
+    for i in a:
+        new_arr.append(i)
+    for i in b:
+        new_arr.append(i)
+    return new_arr
+
+print(merge_arr([1,2,3],[4,5,6]))        
