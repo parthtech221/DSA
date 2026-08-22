f1c3040e2e47387ce23416053bf69e07b14b0347
parthtech221 <parthtech221@gmail.com>
2026-08-22 19:27:48 +0530

    add file

---
+++ 012_second_smallest.py
@@ -0,0 +1,28 @@
+# Problem: find second smallest element
+# Difficulty: Easy
+# Topic: Arrays
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+
+def second_smallest(arr):
+    if len(arr) < 2:
+        return None
+
+    smallest = arr[0]
+    second_smallest = None
+
+    for num in arr[1:]:
+
+        if num < smallest:
+            second_smallest = smallest
+            smallest = num
+
+        elif num != smallest and (
+            second_smallest is None or num < second_smallest
+        ):
+            second_smallest = num
+
+    return second_smallest 
+
+print(second_smallest([5, 2, 8, 1, 9]))
