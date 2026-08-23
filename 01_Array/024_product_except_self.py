5824d221a080989291a73f8ed22c236df7b8157a
parthtech221 <parthtech221@gmail.com>
2026-08-23 15:30:58 +0530

    add file

---
+++ 01_Array/024_product_except_self.py
@@ -0,0 +1,17 @@
+#problem: product of array except self
+# Difficulty: Easy
+# Topic: Arrays
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
+
+def produc_exceptself(arr):
+    new_arr = []
+    for i in range(len(arr)):
+        product = 1
+        for j in range(len(arr)):
+            if i!=j:
+                product*=arr[j]
+        new_arr.append(product)  
+    return new_arr
+      
+print((produc_exceptself([1,2,3,4])))                  
