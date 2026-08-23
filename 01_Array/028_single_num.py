c10facad7cca23f2555ce5c0e5c7bbcb2b82f8aa
parthtech221 <parthtech221@gmail.com>
2026-08-23 17:43:53 +0530

    add file

--- 01_Array/028_single_num.py
+++ 01_Array/028_single_num.py
@@ -5,16 +5,14 @@
 # Space Complexity: O(1)
 
 def find_single(arr):
-    for i in range(len(arr)):
+    for i in arr:
         count=0
-        for j in range(i,len(arr)):
-            if arr[i]==arr[j]:
+        for j in arr:
+            if i==j:
                 count+=1
-            else:
-                element=arr[i]         
-        if count!=0:
-            return element
-        
+        if count==1:
+            return i        
+      
     return None
 
-print(find_single([4,1,2,1,4,3,5,5]))        
+print(find_single([4,1,2,1,4,3,5,5,2,3,6]))        
