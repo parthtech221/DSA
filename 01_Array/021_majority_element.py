4461e9ab0d512c6f64241e1be50b0806abc1f5f0
parthtech221 <parthtech221@gmail.com>
2026-08-23 14:32:47 +0530

    add file

---
+++ 01_Array/021_majority_element.py
@@ -0,0 +1,18 @@
+#problem: majority element 
+# Difficulty: hard
+# Topic: Arrays
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
+
+def majority(arr):
+    for i in arr:
+        count=0
+        for j in arr:
+            if j==i:
+                count+=1
+        if count>len(arr)//2:
+            return i
+    return "no majority element"             
+    
+
+print(majority([1, 2, 2, 2, 3]))            
