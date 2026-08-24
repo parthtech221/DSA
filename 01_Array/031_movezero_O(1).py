c8e2e8a3a8f37a9c84beb8af6154ed4f42249e6b
parthtech221 <parthtech221@gmail.com>
2026-08-24 13:32:50 +0530

    file add

--- 01_Array/031_movezero_O(1).py
+++ 01_Array/031_movezero_O(1).py
@@ -3,3 +3,15 @@
 # Topic: Arrays
 # Time Complexity: O(n)
 # Space Complexity: O(1)
+
+def movexero(arr):
+    n=len(arr)
+    previous = 0
+    for i in range(1,n-1):
+        if arr[previous]!=0 and arr[i]==0:
+            previous+=1
+        if arr[previous]==0 and arr[i]!=0:
+            arr[i],arr[previous]=arr[previous],arr[i]
+    return arr
+                
+print(movexero([1,0,2,0,3,0]))        
