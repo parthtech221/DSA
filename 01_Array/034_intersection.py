c47c8a984b69b6da832b10b17f14ed2c7395bb91
parthtech221 <parthtech221@gmail.com>
2026-08-24 14:20:34 +0530

    add file

---
+++ 01_Array/034_intersection.py
@@ -0,0 +1,15 @@
+# problem: find intersection of two array
+# Difficulty: Easy
+# Topic: Arrays
+# Time Complexity: O(n^2)
+# Space Complexity: O(n)
+
+def intersection(arr1,arr2):
+    new_arr=[]
+    for i in arr1:
+       for j in arr2:
+           if i==j:
+                new_arr.append(j)
+    return new_arr
+
+print(intersection([1,2,3,4],[3,4,5,6]))        
