4f6fd724adfd4c80dfe963397fdcc640cf30f974
parthtech221 <parthtech221@gmail.com>
2026-09-17 11:31:38 +0530

    add file

---
+++ 01_Array/036_towsum_optimal.py
@@ -0,0 +1,11 @@
+# problem: two sum brute force in optimal way
+# Difficulty: Easy
+# Topic: Arrays
+# Time Complexity: 
+# Space Complexity: O(1)
+
+def twosum(arr,target):
+    n = len(arr)
+    for i in arr:
+        
+    
