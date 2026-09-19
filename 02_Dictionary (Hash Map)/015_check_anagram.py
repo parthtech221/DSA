c1b211ff5ee9048b1f1fa366d0cf414b270c0a62
parthtech221 <parthtech221@gmail.com>
2026-09-19 12:08:48 +0530

    add file

--- 02_Dictionary (Hash Map)/015_check_anagram.py
+++ 02_Dictionary (Hash Map)/015_check_anagram.py
@@ -5,15 +5,18 @@
 #Space: O(n)
 
 def check_anagram(string1,string2):
-    check = {}
+    checki = {}
+    checkj = {}
     for i in string1:
-        if i in check:
-            check[i]+=1
+        if i in checki:
+            checki[i]+=1
         else:    
-            check[i]=1
+            checki[i]=1
     for j in string2:
-        if j not in check:
-            return False
-    return True
+        if j in checkj:
+            checkj[j]+=1
+        else:
+            checkj[j]=1    
+    return checki==checkj
 
 print(check_anagram("listen","silent"))            
