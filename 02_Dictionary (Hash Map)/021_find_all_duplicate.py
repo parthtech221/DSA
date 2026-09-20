c0bacc6b86291804f2dabb0788a78803ac85d9ef
parthtech221 <parthtech221@gmail.com>
2026-09-20 14:32:37 +0530

    add file

---
+++ 02_Dictionary (Hash Map)/021_find_all_duplicate.py
@@ -0,0 +1,19 @@
+#Problem: Find all duplicates
+#Difficulty: medium
+#Topic: HashMap
+#Time: O(n)
+#Space: O(n)
+
+def all_duplicate(array):
+    check = {}
+    new_array = []
+    for i in array:
+        if i not in check:
+            check[i]=1
+        else:
+            check[i]+=1    
+        if check[i]>1:
+            new_array.append(i)
+    return new_array
+
+print(all_duplicate([1,2,3,2,4,1,5]))            
