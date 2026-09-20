8582243cee9491b777510a318a6a71b14dc0f944
parthtech221 <parthtech221@gmail.com>
2026-09-20 14:01:55 +0530

    add file

--- 02_Dictionary (Hash Map)/018_remove_duplicate.py
+++ 02_Dictionary (Hash Map)/018_remove_duplicate.py
@@ -8,10 +8,9 @@ def remove_duplicate(array):
     check = {}
     new_array = []
     for i in array:
-        check[i]= True
-        if i in check:
-            new_array.append(i)
-        
+        if i not in check:
+            check[i]= True
+            new_array.append(i)           
     return new_array
 print(remove_duplicate([1,2,2,3,1,4])) 
 print(remove_duplicate([1,1,1,1])) 
