4325d739a5b0fadd45bb55a34031494f398a26b8
parthtech221 <parthtech221@gmail.com>
2026-09-20 15:09:45 +0530

    add file

--- 02_Dictionary (Hash Map)/022_k_freq.py
+++ 02_Dictionary (Hash Map)/022_k_freq.py
@@ -1,19 +1,36 @@
-#Problem: Top K Frequent Elements
-#Difficulty: medium
-#Topic: HashMap
-#Time: O(n)
-#Space: O(n)
+# Problem: Top K Frequent Elements
+# Difficulty: Medium
+# Topic: HashMap
+# Time Complexity: O(n + k*n)
+# Space Complexity: O(n)
 
-def k_freq(array,k):
+def k_freq(array, k):
+
     check = {}
-    new_array = []
+    result = []
+
+    # Build frequency map
     for i in array:
         if i not in check:
-            check[i]=1
+            check[i] = 1
         else:
-            check[i]+=1
-        if (check[i]>= k) and (i not in new_array):
-            new_array.append(i)
-    return new_array
-        
-print(k_freq([1,1,1,2,2,3],2))                    
+            check[i] += 1
+
+    # Find top k elements
+    while len(result) < k and len(check) > 0:
+
+        max_freq = 0
+        max_key = None
+
+        for key in check:
+            if check[key] > max_freq:
+                max_freq = check[key]
+                max_key = key
+
+        result.append(max_key)
+        del check[max_key]
+
+    return result
+
+
+print(k_freq([1,1,1,2,2,3,3,3,3], 2))
