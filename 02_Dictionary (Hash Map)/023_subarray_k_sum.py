f5ef4f198f2e61932f51df955859302594a96594
parthtech221 <parthtech221@gmail.com>
2026-09-20 15:21:35 +0530

    add file

--- 02_Dictionary (Hash Map)/023_subarray_k_sum.py
+++ 02_Dictionary (Hash Map)/023_subarray_k_sum.py
@@ -4,9 +4,24 @@
 #Time: O(n)
 #Space: O(n)
 
-def subarray_sum(array , k):
-    check = {}
-    for i in array:
-        if i not in check:
-            check[i]=True
-            
+def subarray_sum(array, k):
+
+    max_length = 0
+
+    for i in range(len(array)):
+
+        current_sum = 0
+
+        for j in range(i, len(array)):
+
+            current_sum += array[j]
+
+            if current_sum == k:
+
+                length = j - i + 1
+
+                if length > max_length:
+                    max_length = length
+
+    return max_length
+print(subarray_sum([1, -1, 5, -2, 3],3))            
